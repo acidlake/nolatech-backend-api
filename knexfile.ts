a78e0b2b47eb761a07fe258dd3067fbe2795293a
Jeremias Nunez <jeremias2@gmail.com>
2024-07-11 13:32:04 -0400

    feat: added build command

--- knexfile.ts
+++ knexfile.ts
@@ -1,27 +1,52 @@
 import path from "path";
 import dotenv from "dotenv";
+import { Knex } from "knex";
 
 dotenv.config();
 
 const BASE_PATH = path.join(__dirname, "src", "server", "db");
 
-const config = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: process.env.DATABASE_SQLITE_NAME || "./dev.sqlite3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: path.join(BASE_PATH, "migrations"),
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, "seeds"),
-    },
+const development: Knex.Config = {
+  client: "sqlite3",
+  connection: {
+    filename: process.env.DATABASE_SQLITE_NAME || "./dev.sqlite3",
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: path.join(BASE_PATH, "migrations"),
+  },
+  seeds: {
+    directory: path.join(BASE_PATH, "seeds"),
   },
 };
 
+const production: Knex.Config = {
+  client: "pg",
+  version: "7.2",
+  connection: {
+    connectionString: process.env.DATABASE_CONNECTION_STRING,
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: process.env.DATABASE_SSL ? { rejectUnauthorized: false } : false,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: path.join(BASE_PATH, "migrations"),
+  },
+  seeds: {
+    directory: path.join(BASE_PATH, "seeds"),
+  },
+};
+
+const config = {
+  development,
+  production,
+};
+
 const environment = process.env.NODE_ENV || "development";
-const knexConfig = config.development;
+const knexConfig: Knex.Config = config[environment as keyof typeof config];
 
 export default knexConfig;
--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import { knex } from "knex";
-import config from "../knexfile";
+const config = require("../knexfile");
 
 const db = knex(config);
 export default db;
